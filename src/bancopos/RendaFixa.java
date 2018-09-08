f5ddd94ca9d1496c7ca73b46f51341a902443075
ElisonKs <elison.nunes@gmail.com>
2018-09-08 10:16:21 -0300

    Alterações Nailson

--- src/bancopos/RendaFixa.java
+++ src/bancopos/RendaFixa.java
@@ -5,28 +5,9 @@ public class RendaFixa extends Investimento {
     
     private final double rendFixo = 0.008;
     
-
     public RendaFixa(double valorInvestido){
         setRendimento(rendFixo); 
         setSaldo(valorInvestido);
-
-    private double saldo;
-    
-    public RendaFixa(double valorInvestido){
-        this.saldo = valorInvestido;
-    }
-    
-    public void render(){
-        this.saldo += (this.saldo * this.rendimento);
-    }
-
-    public double getSaldo() {
-        return saldo;
-    }
-
-    public void setSaldo(double saldo) {
-        this.saldo = saldo;
-
     }
     
     @Override
--- src/bancopos/RendaVariavel.java
+++ src/bancopos/RendaVariavel.java
@@ -1,7 +1,6 @@
 package bancopos;
 import java.lang.Math;
 
-
 public class RendaVariavel extends Investimento {
     
     public RendaVariavel(double valorInvestido)
@@ -15,38 +14,5 @@ public class RendaVariavel extends Investimento {
         //saldo += (saldo * rendimento);
         setSaldo(getSaldo()+(getSaldo()*getRendimento()));
        
-
-public class RendaVariavel {
-    
-    private double rendimento;
-    private double saldo;
-    
-    public RendaVariavel(double valorInvestido)
-    {
-        this.rendimento = Math.random();
-        this.saldo = valorInvestido;
-
     }
-    
-    public void render(){
-        this.saldo += (this.saldo * this.rendimento);
-    }
-
-    public double getRendimento() {
-        return rendimento;
-    }
-
-    public void setRendimento(double rendimento) {
-        this.rendimento = rendimento;
-    }
-
-    public double getSaldo() {
-        return saldo;
-    }
-
-    public void setSaldo(double saldo) {
-        this.saldo = saldo;
-    }
-    
-    
 }
