441f64b2dc005019fa2f642345ed1bd96fce04e9
ElisonKs <elison.nunes@gmail.com>
2018-09-09 16:30:00 -0300

    Extrato e Argumentos Variáveis

--- src/bancopos/BancoPos.java
+++ src/bancopos/BancoPos.java
@@ -7,7 +7,9 @@ package bancopos;
 
 import java.io.IOException;
 import java.text.ParseException;
+import java.time.LocalTime;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -19,9 +21,22 @@ import javax.swing.JOptionPane;
  */
 public class BancoPos {
 
-    public static void menu()
-    {
-        System.out.println("");
+    static ArrayList<Cliente_PessoaFisica> ListaClientesPF = new ArrayList();
+    static ArrayList<Cliente_PessoaJuridica> ListaClientesPJ = new ArrayList();
+    static ArrayList<Transacao> ListaTransacoes = new ArrayList();
+
+    public static int menu() {
+        String aux;
+        aux = JOptionPane.showInputDialog(null, "Selecione a opção desejada:\n"
+                + "1 - Imprimir Extrato\n"
+                + "2 - Verificar Saldo\n"
+                + "3 - Sacar\n"
+                + "4 - Transferir\n"
+                + "5 - Depositar\n"
+                + "6 - Investir\n"
+                + "7 - Encerrar", "Menu", 3);
+
+        /*   System.out.println("");
         System.out.println("Menu");
         System.out.println("1 - Cadastrar Cliente");
         System.out.println("2 - Verificar Saldo");
@@ -29,60 +44,81 @@ public class BancoPos {
         System.out.println("4 - Transferir");
         System.out.println("5 - Depositar");
         System.out.println("6 - Investir");
-        System.out.println("7 - Encerrar");
+        System.out.println("7 - Encerrar");*/
+        return Integer.parseInt(aux);
     }
+
     public static void main(String[] args) throws ParseException {
-        
-       int opcao = 0;
-       ArrayList<Cliente_PessoaFisica> ListaClientesPF = new ArrayList();
-       ArrayList<Cliente_PessoaJuridica> ListaClientesPJ = new ArrayList();
-       Scanner leitor = new Scanner(System.in); 
-       
-      
-       while(opcao != 7)
-       {
-           menu();
-           opcao = leitor.nextInt();
+
+        int opcao = 0;
+
+        Scanner leitor = new Scanner(System.in);
+
+        while (opcao != 7) {
+            opcao = menu();
            
-           switch(opcao)
-           {
-               case 1:
-                   int aux;
-                   System.out.println("Informe 1 para Pessoa Fisica e 2 para Pessoa Juridica:");
-                   aux = leitor.nextInt();
-                   if(aux == 1)
-                   {
-                       Cliente_PessoaFisica pf1,pf2;
-                       pf1 = new Cliente_PessoaFisica("02860189505","Elison Nunes",1,"Rua do IMperador 76","elison.nunes@gmail.com","87988540970");
-                       Agencia agencia_aux = new Agencia(969,"Rua das Flores 25","8738664567");
-                       ContaCorrente contacorrenteAux = new ContaCorrente(1,agencia_aux,3500);
-                       ContaPoupanca contaPoupancaAux = new ContaPoupanca(2,agencia_aux);
-                       pf1.contasCliente.add(contacorrenteAux);
-                       pf1.contasCliente.add(contaPoupancaAux);
-                       ListaClientesPF.add(pf1);
-                       
-                       pf2 = new Cliente_PessoaFisica("9505","Vinicius Barbosa",2,"Rua do Coqueiro 25","alenvi@gmail.com","8798854555");
-                       agencia_aux = new Agencia(969,"Rua das Flores 25","8738664567");
-                        contacorrenteAux = new ContaCorrente(3,agencia_aux,4000);
+
+            switch (opcao) {
+                case 1:
+                    int aux;
+                    System.out.println("Informe 1 para Pessoa Fisica e 2 para Pessoa Juridica:");
+                    aux = leitor.nextInt();
+                    if (aux == 1) {
+                        Cliente pf1, pf2;
+                        pf1 = new Cliente_PessoaFisica("02860189505", "Elison Nunes", 1, "Rua do IMperador 76", "elison.nunes@gmail.com", "87988540970");
+                        Agencia agencia_aux = new Agencia(969, "Rua das Flores 25", "8738664567");
+                        ContaCorrente contacorrenteAux = new ContaCorrente(1, agencia_aux, 3500);
+                        ContaPoupanca contaPoupancaAux = new ContaPoupanca(2, agencia_aux);
+                        pf1.contasCliente.add(contacorrenteAux);
+                        pf1.contasCliente.add(contaPoupancaAux);
+                        ListaClientesPF.add((Cliente_PessoaFisica) pf1);
+
+                        pf2 = new Cliente_PessoaFisica("9505", "Vinicius Barbosa", 2, "Rua do Coqueiro 25", "alenvi@gmail.com", "8798854555");
+                        agencia_aux = new Agencia(969, "Rua das Flores 25", "8738664567");
+                        contacorrenteAux = new ContaCorrente(3, agencia_aux, 4000);
                         pf2.contasCliente.add(contacorrenteAux);
-                    
-                       ListaClientesPF.add(pf2);
-                   }
-                   break;
-               case 7:
-                   LerEscreverArquivo salvar = new LerEscreverArquivo();
-                   {
-                     try {
-                         salvar.gravarSaldosPF(ListaClientesPF);
-                          } catch (IOException ex) {
-                      Logger.getLogger(BancoPos.class.getName()).log(Level.SEVERE, null, ex);
+
+                        ListaClientesPF.add((Cliente_PessoaFisica) pf2);
+                        Date data_aux = new Date(2018,9,9);
+                        Transacao transacao_aux1 = new Transacao(1,2,200,TipoTransacao.TRANSFERENCIA,data_aux);
+                        ListaTransacoes.add(transacao_aux1);
+                        data_aux = new Date(2018,8,9);
+                         Transacao transacao_aux2 = new Transacao(1,200,TipoTransacao.SAQUE,data_aux);
+                         ListaTransacoes.add(transacao_aux2);
+                    }   
+                    break;
+                case 2:
+                    Date data1 = new Date(2018,8,9);
+                    imprimirExtrato(data1);
+                    Date data2 = new Date(2018,9,9);
+                    imprimirExtrato(data1,data2);
+                    break;
+                case 7:
+                    LerEscreverArquivo salvar = new LerEscreverArquivo();
+                     {
+                        try {
+                            salvar.gravarSaldosPF(ListaClientesPF);
+                        } catch (IOException ex) {
+                            Logger.getLogger(BancoPos.class.getName()).log(Level.SEVERE, null, ex);
+                        }
                     }
-                   }
-                   break;
-                           
-           }
-           
-       }
+                    break;
+
+            }
+
+        }
     }
-    
+
+    public static void imprimirExtrato(Date... datas) {
+        System.out.println("Entrou na funcao");
+        for (Date data : datas) {
+          for(Transacao transacao : ListaTransacoes)
+          {
+              if((transacao.getDataTransacao().compareTo(data)==0))
+                  transacao.imprimir();
+          }
+        }
+
+    }
+
 }
---
+++ src/bancopos/Transacao.java
@@ -0,0 +1,88 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bancopos;
+
+import java.util.Date;
+
+
+/**
+ *
+ * @author Elison
+ */
+public class Transacao {
+    
+    private int numeroContaOrigem;
+    private int numeroContaDestino;
+    private double valorTransacao;
+    private TipoTransacao tipoTransacao;
+    private Date dataTransacao;
+
+    public Transacao(int numeroContaOrigem, int numeroContaDestino, double valorTransacao, TipoTransacao tipoTransacao, Date dataTransacao) {
+        this.numeroContaOrigem = numeroContaOrigem;
+        this.numeroContaDestino = numeroContaDestino;
+        this.valorTransacao = valorTransacao;
+        this.tipoTransacao = tipoTransacao;
+        this.dataTransacao = dataTransacao;
+    }
+
+     public Transacao(int numeroContaOrigem,  double valorTransacao, TipoTransacao tipoTransacao, Date dataTransacao) {
+        this.numeroContaOrigem = numeroContaOrigem;
+        this.valorTransacao = valorTransacao;
+        this.tipoTransacao = tipoTransacao;
+        this.dataTransacao = dataTransacao;
+    }
+    
+    public int getNumeroContaOrigem() {
+        return numeroContaOrigem;
+    }
+
+    public void setNumeroContaOrigem(int numeroContaOrigem) {
+        this.numeroContaOrigem = numeroContaOrigem;
+    }
+
+    public int getNumeroContaDestino() {
+        return numeroContaDestino;
+    }
+
+    public void setNumeroContaDestino(int numeroContaDestino) {
+        this.numeroContaDestino = numeroContaDestino;
+    }
+
+    public double getValorTransacao() {
+        return valorTransacao;
+    }
+
+    public void setValorTransacao(double valorTransacao) {
+        this.valorTransacao = valorTransacao;
+    }
+
+    public TipoTransacao getTipoTransacao() {
+        return tipoTransacao;
+    }
+
+    public void setTipoTransacao(TipoTransacao tipoTransacao) {
+        this.tipoTransacao = tipoTransacao;
+    }
+
+    public Date getDataTransacao() {
+        return dataTransacao;
+    }
+
+    public void setDataTransacao(Date dataTransacao) {
+        this.dataTransacao = dataTransacao;
+    }
+    
+    public void imprimir()
+    {
+        System.out.println("Data:" + getDataTransacao());
+        System.out.println("Conta Origem:" + getNumeroContaOrigem());
+        System.out.println("Conta Destino:" + getNumeroContaDestino());
+        System.out.println("Tipo:" + getTipoTransacao().toString());
+        System.out.println("Valor:" + getValorTransacao());
+                           
+    }
+    
+}
